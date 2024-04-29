c77e1f6d5a513b0531e2d6bbf30276b9bf15c8b4
Leticia Torres Soares Martins <leticia@MacBook-Air-de-Leticia.local>
2024-04-29 17:08:45 -0300

    Estilização da tela de cadastro

--- vue-app/src/components/CadastroComponent.vue
+++ vue-app/src/components/CadastroComponent.vue
@@ -1,106 +1,177 @@
 <template>
-    <div class="overlay">
-        <div class="card">
-                <form @submit.prevent="handleCadastro" className='form'>
-                    <div className='inputs'>
-                        <div className='nome'>
-                            <label className='nome-cadastro'>Nome</label>
-                            <input placeholder='Digite seu nome' className='form-control' v-model="formData.nome"/>
-                        </div>
-
-                        <div className='sobrenome'>
-                            <label className='sobrenome-cadastro'>Sobrenome</label>
-                            <input placeholder='Digite seu sobrenome' className='form-control' v-model="formData.sobrenome"/>
-                        </div>
-
-                        <div className='email'>
-                            <label className='email-login'>E-mail</label>
-                            <input type='email' placeholder='Insira o seu e-mail' className='form-control' v-model="formData.email"/>
-                        </div>
-
-                        <div className='senha-login'>
-                            <label className='senha'>Senha</label>
-                            <input type='password' placeholder='Insira sua senha' className='form-control' v-model="formData.senha"/>
-                        </div>
-                        <div className='curso'>
-                            <label className='curso-cadastro'>Curso</label>
-                            <input placeholder='Insira o seu curso' className='form-control' v-model="formData.curso"/>
-                        </div>
-
-                        <div className='matricula-cadastro'>
-                            <label className='matricula'>Matrícula</label>
-                            <input placeholder='Insira sua matricula' className='form-control' v-model="formData.matricula"/>
-                        </div>
-                    </div>
-                    <button type='submit' className='login-button'>Cadastrar</button>
-                </form>
+    <div class="container-fluid p-0">
+      <div class="row m-0">
+        <!-- Tela inicial branca ocupando a tela inteira -->
+        <div class="col-lg-6 d-none d-lg-block"></div>
+  
+        <!-- Frame para realizar o cadastro -->
+        <div class="col-lg-6 px-4 py-5 bg-blue">
+          <h1 class="title">Cadastre-se</h1>
+          <div class="card">
+            <form @submit.prevent="handleCadastro" class="form">
+              <div class="inputs">
+                <!-- Div para agrupar os campos Nome e CPF -->
+                <div class="form-group">
+                  <div class="form-group">
+                    <label for="nome" class="mr-flex"></label>
+                    <input id="nome" v-model="formData.nome" class="form-control" placeholder="Nome" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="CPF" class="mr-2"></label>
+                    <input id="CPF" v-model="formData.CPF" class="form-control" placeholder="CPF" required>
+                  </div>
+                </div>
+                <!-- Fim do grupo Nome e CPF -->
+                <div class="form-group">
+                <label for="email" class="mr-2 d-flex"></label>
+                <input id="email" v-model="formData.email" type="email" class="form-control" placeholder="Insira o seu e-mail" required>
+              </div>
+                <!-- Div para agrupar os campos Senha e Confirmação de Senha -->
+                <div class="form-group">
+                  <div class="form-group">
+                    <label for="senha" class="mr-2"></label>
+                    <input id="senha" v-model="formData.senha" type="password" class="form-control" placeholder="Insira sua senha" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="senhaConfirme" class="mr-2"></label>
+                    <input id="senhaConfirme" v-model="formData.senhaConfirme" type="password" class="form-control" placeholder="Confirme sua senha" required>
+                  </div>
+                </div>
+                <!-- Fim do grupo Senha e Confirmação de Senha -->
+  
+                <!-- Div para agrupar os campos Curso e Matrícula -->
+                <div class="form-group">
+                  <div class="form-group">
+                    <label for="curso" class="mr-2"></label>
+                    <input id="curso" v-model="formData.curso" class="form-control" placeholder="Insira o seu curso" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="matricula" class="mr-2"></label>
+                    <input id="matricula" v-model="formData.matricula" class="form-control" placeholder="Insira sua matrícula" required>
+                  </div>
+                </div>
+                <!-- Fim do grupo Curso e Matrícula -->
+  
+                <button type="submit" class="btn btn-primary btn-block">Cadastrar</button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  
 
 <script>
-
 export default {
     name: "CadastroComponent",
     data() {
         return {
             formData: {
-                nome: '',
-                sobrenome: '',
-                email: '',
-                senha: '',
-                curso: '',
-                matricula: ''
+                nome: "",
+                CPF: "",
+                email: "",
+                senha: "",
+                senhaConfirme:"",
+                curso: "",
+                matricula: ""
             }
-            
-        }
+        };
     },
     methods: {
         async handleCadastro() {
             try {
-                const response = await fetch('http://localhost:3000/', {
-                    method: 'POST',
+                const response = await fetch("http://localhost:3000/", {
+                    method: "POST",
                     headers: {
-                        'Content-Type': 'application/json'
+                        "Content-Type": "application/json"
                     },
                     body: JSON.stringify(this.formData)
                 });
                 const data = await response.json();
-                console.log('Resposta do servidor:', data);
+                console.log("Resposta do servidor:", data);
                 // Limpar o formulário após o envio bem-sucedido
-                this.formData.nome = '';
-                this.formData.sobrenome = '';
-                this.formData.email = '';
-                this.formData.senha = '';
-                this.formData.curso = '';
-                this.formData.matricula = '';
+                this.formData.nome = "";
+                this.formData.sobrenome = "";
+                this.formData.email = "";
+                this.formData.senha = "";
+                this.formData.curso = "";
+                this.formData.matricula = "";
             } catch (error) {
-                console.error('Erro ao cadastrar:', error);
+                console.error("Erro ao cadastrar:", error);
                 // Tratar erros de requisição
             }
         }
-    },
-    mounted() {
-        document.body.style.backgroundColor = 'gray';
-        document.body.style.display = 'flex';
-        document.body.style.justifyContent = 'center';
-        document.body.style.alignItems = 'center';
-        document.body.style.height = '100vh';
-        document.body.style.flexDirection = 'column';
-        document.body.style.overflow = 'hidden';
-    },
-    beforeUnmount() {
-        document.body.style.backgroundColor = null;
     }
-}
+};
 </script>
 
 <style scoped>
-.card{
-    background-color: white;
-}
-.h2-login {
-    color: blue;
+.container {
+  display: flex;
 }
 
+.bg-white {
+  flex: 1; /* Ocupa todo o espaço restante */
+}
+
+.bg-blue {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 45%; /* Largura do elemento */
+  height: 100%;
+  background: linear-gradient(110deg, #102C46 0%, #085C48 49%, #018A4A 95%);
+}
+
+.card {
+  position: absolute;
+  top: 55%;
+  right: 15%; /* Ajuste fino para espaçamento da borda direita */
+  transform: translate(0, -50%);
+  width: 45%; /* Largura do elemento */
+  height: 496px;
+  background: linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.1));
+  border-radius: 8px;
+  padding: 50px;
+  margin-bottom: 50px;
+}
+
+
+/* Estilos adicionais para o título "Cadastre-se" */
+.title {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 64px;
+  color: #fff; /* Cor branca */
+  padding: 20px; /* Padding para todos os lados */
+  margin-bottom: 20px; /* Margem inferior para criar espaço entre o título e a caixa do card */
+  margin-top: 20px;
+}
+
+/* Estilos adicionais para os outros textos */
+.text {
+  font-family: 'Inter', sans-serif;
+  font-size: 14px;
+  color: #6D6B71; /* Cor cinza */
+}
+.inputs {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Centralizar itens horizontalmente */
+}
+
+.form-group {
+  margin-bottom: 12px; /* Espaçamento na parte inferior de cada campo */
+  display: flex;
+  flex-direction: row;
+  align-items: center; /* Centralizar itens horizontalmente */
+}
+
+.form-group input {
+  margin-right: 10px; /* Espaçamento entre os campos */
+  border-radius: 12px;
+  border: none; /* Remove a sombra */
+  padding: 12px;
+}
 </style>
