c6dd6ac1d1edc29c9406ec1f880c862ed2d464a1
Mateus Magno <mateusmagnoferreirasoares@gmail.com>
2024-05-02 20:17:57 -0300

    Alterações Front-End

--- vue-app/src/components/CadastroComponent.vue
+++ vue-app/src/components/CadastroComponent.vue
@@ -12,34 +12,43 @@
             <div class="inputs">
               <!-- Div para agrupar os campos Nome e CPF -->
               <div class="form-group">
-                <div class="form-group">
+                <!-- element agrupa a label e o input para o campo do forum-->
+                <div class="element">
                   <label for="nome" class="mr-flex"></label>
                   <input id="nome" v-model="formData.nome" class="form-control" placeholder="Nome" required>
                 </div>
-                <div class="form-group">
+                <div class="element">
                   <label for="sobrenome" class="mr-flex"></label>
                   <input id="sobrenome" v-model="formData.sobrenome" class="form-control" placeholder="Sobrenome" required>
                 </div>
               </div>
               <!-- Fim do grupo Nome e Sobrenome -->
-
+              
+              <!-- Div para agrupar CPF e Email-->
               <div class="form-group">
-                <div class="form-group">
+
+                <div class="element">
                   <label for="cpf" class="mr-flex"></label>
                   <input id="cpf" v-model="formData.cpf" @input="formatarCPF" class="form-control" placeholder="CPF" required>
                 </div>
-                <div class="form-group">
+
+                <div class="element">
                   <label for="email" class="d-flex"></label>
-                  <input id="email" v-model="formData.email" type="email" class="email" placeholder="E-mail" required>
+                  <input id="email" v-model="formData.email" type="email" class="form-control" placeholder="E-mail" required>
                 </div>
+              
               </div>
+              <!-- Fim do grupo CPF e Email-->
+
               <!-- Div para agrupar os campos Senha e Confirmação de Senha -->
               <div class="form-group">
-                <div class="form-group">
+
+                <div class="element">
                   <label for="senha" class="mr-2"></label>
                   <input id="senha" v-model="formData.senha" type="password" class="form-control" placeholder="Senha" required>
                 </div>
-                <div class="form-group">
+
+                <div class="element">
                   <label for="confirma_senha" class="mr-2"></label>
                   <input id="confirma_senha" v-model="confirmacao_senha" type="password" class="form-control" placeholder="Confirme sua senha" required>
                 </div>
@@ -48,11 +57,13 @@
 
               <!-- Div para agrupar os campos Curso e Matrícula -->
               <div class="form-group">
-                <div class="form-group">
+
+                <div class="element">
                   <label for="curso" class="mr-2"></label>
                   <input id="curso" v-model="formData.curso" class="form-control" placeholder="Curso" required>
                 </div>
-                <div class="form-group">
+
+                <div class="element">
                   <label for="matricula" class="mr-2"></label>
                   <input id="matricula" v-model="formData.matricula" class="form-control" placeholder="Matrícula" required>
                 </div>
@@ -158,27 +169,27 @@ export default {
 
 .card {
   position: absolute;
-  top: 55%;
-  right: 19%; /* Ajuste fino para espaçamento da borda direita */
-  transform: translate(0, -50%);
+  top: 50%; /* Define o topo como 50% da altura da div pai */
+  left: 50%; /* Define a margem esquerda como 50% da largura da div pai */
+  transform: translate(-50%, -50%); /* Move o elemento de volta metade de sua largura e metade de sua altura */
   width: 55%; /* Largura do elemento */
   height: 45%;
   background: linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.1));
   border-radius: 12px;
-  padding: 20px;
+  padding: 6%;
   margin-bottom: 50px;
 }
 
 
 /* Estilos adicionais para o título "Cadastre-se" */
 .title {
+  position: absolute; /* Define o título como posição absoluta */
+  top: calc(50% - 46%); /* Ajusta a posição vertical do topo do título */
+  left: calc(50% - 36%); /* Ajusta a posição horizontal para alinhar com o card */
   font-family: 'Open Sans', sans-serif;
-  font-size: 450%;
+  font-size: 400%;
   color: #fff; /* Cor branca */
   padding: 20px; /* Padding para todos os lados */    
-  margin-bottom: 55px; /* Margem inferior para criar espaço entre o título e a caixa do card */
-  margin-top: 140px;
-  margin-left: 19%;
 }
 
 /* Estilos adicionais para os outros textos */
@@ -188,31 +199,50 @@ export default {
   letter-spacing: 5%;
   color: #6D6B71; /* Cor cinza */
 }
+
 .inputs {
-  margin-top: 40px;
-  margin-left: 30px;
-  flex-direction: column;
-  align-items: center; /* Centralizar itens horizontalmente */
+  margin-top: 9%;
+  display: grid; /* Usar grid layout */
+  grid-template-columns: repeat(3); /* Duas colunas com largura igual */
+  grid-template-rows: repeat(2); /* Três linhas com altura automática */
+  align-self: center; /* Centralizar verticalmente */
+  justify-content: center;
+  gap: 5%;
 }
+
+
 
 .form-group {
-  margin-bottom: 16px; /* Espaçamento na parte inferior de cada campo */
-  display: flex;
-  align-items: center; /* Centralizar itens horizontalmente */
+  display: flex; /* Use flexbox para organizar os elementos lado a lado */
+  justify-content: center;
+  margin-bottom: 5%;
 }
 
-.form-group input {
-  margin-right: 20px; /* Espaçamento entre os campos */
+.element {
+  display: flex;
+  justify-content: center;
+  width: 150%;
+  margin-right: 18%;
+}
+
+.form-group .element:last-child{
+  margin-right: 0%;
+}
+
+.form-control{
   border-radius: 12px;
   border: none; /* Remove a sombra */
-  padding: 12px;
+  padding: 8%;
+  align-self: center; /* Centralizar verticalmente */
+  justify-content: center;
 }
 
+
 .btn-cadastrar {
-  margin-right: 10px; /* Espaçamento entre os campos */
+  margin-right: 5%; /* Espaçamento entre os campos */
   width: 120px; /* Defina a largura desejada para o botão */
   height: fit-content; /* Defina a altura desejada para o botão */
-  padding: 12px;
+  padding: 4%;
   background-color: #102C46; /* Cor de fundo do botão */
   color: white; /* Cor do texto do botão */
   border: none; /* Remove a borda do botão */
@@ -221,6 +251,7 @@ export default {
   font-family: 'Open Sans', sans-serif;
   cursor: pointer; /* Altera o cursor ao passar o mouse sobre o botão */
   transition: background-color 0.3s ease; /* Adiciona uma transição suave para a cor de fundo */
+  
 }
 
 .btn-cadastrar:hover {
@@ -228,9 +259,10 @@ export default {
 }
 
 .btn-cancelar {
+
   width: 120px; /* Defina a largura desejada para o botão */
   height: fit-content; /* Defina a altura desejada para o botão */
-  padding: 12px;
+  padding: 4%;
   background: linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.1));
   color: white; /* Cor do texto do botão */
   border: none; /* Remove a borda do botão */
