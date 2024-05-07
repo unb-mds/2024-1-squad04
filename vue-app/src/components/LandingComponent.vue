922193a6c8035e1bdbad1d2e7ea71313e1be2da6
Mateus Magno <mateusmagnoferreirasoares@gmail.com>
2024-05-07 19:55:35 -0300

    landing page

--- vue-app/src/components/LandingComponent.vue
+++ vue-app/src/components/LandingComponent.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class = body>
-        <header class="scrollednav">
-            <nav>
-                <div class="branding">
-                    <h1 class="logo">AvaliaUnB</h1>
-                </div>
-                <ul class="navegation">
-                    <button class="loginbtn" @click.prevent="HandleLogin">ENTRAR</button>
-                </ul>
-            </nav>
-        </header>
+    <div class="container-fluid p-0">
         
-        
+            <div class = "landing-container" style = "margin:0">
+                <header class="scrollednav">
+                    <nav>
+                        <div class="branding">
+                            <h1 class="logo">AvaliaUnB</h1>
+                        </div>
+                        <ul class="navegation">
+                            <button class="loginbtn" @click.prevent="HandleLogin">ENTRAR</button>
+                        </ul>
+                    </nav>
+                </header>
+                
+                
+            </div> 
     </div>
     
 </template>
@@ -50,24 +53,21 @@
 
 <style scoped>
 
-    .body{
-        background: linear-gradient(149deg, #102C46 25%, #085C48 60%);
-    }
+    
 
     .logo{
         background: linear-gradient(149deg, #102C46 25%, #085C48 60%);
         color: transparent; /* Torna o texto transparente para mostrar o gradiente */
         background-clip: text;
+        
     }
 
-    .header{
-        background-color: rgba(0, 0, 0, 0.8);
-        z-index: 99;
-        width: 100%;
-        position: fix;
-        transition: .5s ease all;
-        color: #fff;
+    .landing-container {
+        background: linear-gradient(149deg, #102C46 25%, #085C48 60%); /* Cinza claro */
+        margin: none;
+        min-height: 100vh; /* Garante que o plano de fundo se estenda até o fundo da página */
     }
+
 
     .nav{
         display: flex;
@@ -80,7 +80,12 @@
     }
 
     .scrollednav{
-        background: #333;
+        background: #FAFAFA;
+        z-index: 99;
+        width: 100%;
+        position: fix;
+        transition: .5s ease all;
+        left: 0;
     }
 
     .navegation{
