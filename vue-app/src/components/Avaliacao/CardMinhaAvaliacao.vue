3b6335c76d52eb39ce46596379eeb1acfdd4f0c5
Leticia Torres Soares Martins <leticia@MacBook-Air-de-Leticia.local>
2024-06-03 15:41:33 -0300

    Melhora na estilização da pagina e resposividade

--- vue-app/src/components/Avaliacao/CardMinhaAvaliacao.vue
+++ vue-app/src/components/Avaliacao/CardMinhaAvaliacao.vue
@@ -1,30 +1,54 @@
 <template>
   <div class="comment-card">
-    <div class="materia-info">
-      <span class="materia">Materia</span>
+    <div class="card-header">
+      <div class="materia-info">
+        <span class="materia">Materia</span>
+      </div>
+      <button class="trash-button">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path class="trash-icon" d="M21.07 5.23C19.46 5.07 17.85 4.95 16.23 4.86V4.85L16.01 3.55C15.86 2.63 15.64 1.25 13.3 1.25H10.68C8.35004 1.25 8.13003 2.57 7.97004 3.54L7.76004 4.82C6.83004 4.88 5.90004 4.94 4.97004 5.03L2.93004 5.23C2.51004 5.27 2.21004 5.64 2.25004 6.05C2.29004 6.46 2.65004 6.76 3.07004 6.72L5.11004 6.52C10.35 6 15.63 6.2 20.93 6.73C20.96 6.73 20.98 6.73 21.01 6.73C21.39 6.73 21.72 6.44 21.76 6.05C21.79 5.64 21.49 5.27 21.07 5.23Z" fill="#171717" fill-opacity="0.5"/>
+          <path class="trash-icon" d="M19.23 8.14C18.99 7.89 18.66 7.75 18.32 7.75H5.67999C5.33999 7.75 4.99999 7.89 4.76999 8.14C4.53999 8.39 4.40999 8.73 4.42999 9.08L5.04999 19.34C5.15999 20.86 5.29999 22.76 8.78999 22.76H15.21C18.7 22.76 18.84 20.87 18.95 19.34L19.57 9.09C19.59 8.73 19.46 8.39 19.23 8.14ZM13.66 17.75H10.33C9.91999 17.75 9.57999 17.41 9.57999 17C9.57999 16.59 9.91999 16.25 10.33 16.25H13.66C14.07 16.25 14.41 16.59 14.41 17C14.41 17.41 14.07 17.75 13.66 17.75ZM14.5 13.75H9.49999C9.08999 13.75 8.74999 13.41 8.74999 13C8.74999 12.59 9.08999 12.25 9.49999 12.25H14.5C14.91 12.25 15.25 12.59 15.25 13C15.25 13.41 14.91 13.75 14.5 13.75Z" fill="#171717" fill-opacity="0.5"/>
+        </svg>
+      </button>
     </div>
     <div class="professor-info">
       <span class="professor">Professor</span>
     </div>
-    <div class="flex-container">
-      <div class="five-estrelas">
-        <img
-          v-for="(index) in 5"
-          :key="index"
-          ref="estrelas"
-          src="../../assets/icons/avaliacao/icone-estrela-azul.svg"
-          alt=""
-          class="estrela"
-        />
-      </div>
-      <div class="comment-data">
-        <span class="date">1 de junho de 2024</span>
-      </div>
+    <div class="five-estrelas">
+      <img
+        v-for="(index) in 5"
+        :key="index"
+        ref="estrelas"
+        src="../../assets/icons/avaliacao/icone-estrela-azul.svg"
+        alt=""
+        class="estrela"
+      />
     </div>
+
     <div class="comment-content">
       <p class="comment-text">
-        "Adorei a disciplina de Metódos de Desenvolvimento de Software. Os projetos práticos nos desafiam a aplicar conceitos teóricos de forma eficiente."      </p>
-      <button class="like-button">Apagar</button>
+        "Adorei a disciplina de Metódos de Desenvolvimento de Software. Os projetos práticos nos desafiam a aplicar conceitos teóricos de forma eficiente."
+      </p>
+    </div>
+    <div class="reaction-buttons">
+      <div class="reaction">
+        <button class="like-button">
+          <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.94287 13.0973V5.90059C5.94287 5.61726 6.02787 5.34101 6.1837 5.10726L8.11745 2.23143C8.42204 1.77101 9.17995 1.44518 9.82454 1.68601C10.5187 1.91976 10.9791 2.69893 10.8304 3.39309L10.462 5.70934C10.4337 5.92184 10.4904 6.11309 10.6108 6.26184C10.7312 6.39643 10.9083 6.48143 11.0995 6.48143H14.0108C14.5704 6.48143 15.052 6.70809 15.3354 7.10476C15.6045 7.48726 15.6541 7.98309 15.477 8.48601L13.7345 13.7914C13.515 14.6698 12.5587 15.3852 11.6095 15.3852H8.84704C8.37245 15.3852 7.70662 15.2223 7.40204 14.9177L6.49537 14.2164C6.14829 13.9543 5.94287 13.5364 5.94287 13.0973Z" fill="#171717" fill-opacity="0.5"/>
+            <path d="M3.6905 4.51953H2.96091C1.863 4.51953 1.41675 4.94453 1.41675 5.99286V13.1187C1.41675 14.167 1.863 14.592 2.96091 14.592H3.6905C4.78841 14.592 5.23466 14.167 5.23466 13.1187V5.99286C5.23466 4.94453 4.78841 4.51953 3.6905 4.51953Z" fill="#171717" fill-opacity="0.5"/>
+          </svg>
+        </button>
+        <span class="like-count">25</span>
+      </div>
+      <div class="reaction">
+        <button class="deslike-button">
+          <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="rotated">
+            <path d="M5.94287 13.0973V5.90059C5.94287 5.61726 6.02787 5.34101 6.1837 5.10726L8.11745 2.23143C8.42204 1.77101 9.17995 1.44518 9.82454 1.68601C10.5187 1.91976 10.9791 2.69893 10.8304 3.39309L10.462 5.70934C10.4337 5.92184 10.4904 6.11309 10.6108 6.26184C10.7312 6.39643 10.9083 6.48143 11.0995 6.48143H14.0108C14.5704 6.48143 15.052 6.70809 15.3354 7.10476C15.6045 7.48726 15.6541 7.98309 15.477 8.48601L13.7345 13.7914C13.515 14.6698 12.5587 15.3852 11.6095 15.3852H8.84704C8.37245 15.3852 7.70662 15.2223 7.40204 14.9177L6.49537 14.2164C6.14829 13.9543 5.94287 13.5364 5.94287 13.0973Z" fill="#171717" fill-opacity="0.5"/>
+            <path d="M3.6905 4.51953H2.96091C1.863 4.51953 1.41675 4.94453 1.41675 5.99286V13.1187C1.41675 14.167 1.863 14.592 2.96091 14.592H3.6905C4.78841 14.592 5.23466 14.167 5.23466 13.1187V5.99286C5.23466 4.94453 4.78841 4.51953 3.6905 4.51953Z" fill="#171717" fill-opacity="0.5"/>
+          </svg>
+        </button>
+        <span class="deslike-count">14</span>
+      </div>
     </div>
   </div>
 </template>
@@ -43,12 +67,19 @@ export default {
   padding: 10px;
   margin-bottom: 20px;
   width: 434px;
+  width: 90%; /* Definindo a largura como 100% para ocupar todo o espaço disponível */
+  max-width: 600px; /* Limitando a largura máxima para que o card não fique muito largo */
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .materia-info {
   display: flex;
   align-items: center;
-  margin-bottom: 10px;
 }
 
 .materia {
@@ -67,7 +98,7 @@ export default {
 .professor {
   font-weight: lighter;
   font-family: 'Inter', sans-serif;
-  font-size: 10px;
+  font-size: 12px;
   color: #333333 65%;
 }
 
@@ -93,22 +124,57 @@ export default {
   font-weight: lighter;
 }
 
-.comment-data {
+.trash-button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 5px;
   display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.trash-icon {
+  transition: fill 0.3s ease;
+}
+
+.trash-button:hover .trash-icon {
+  fill: #858585;
+}
+
+.reaction-buttons {
+  display: flex;
+  justify-content: flex-start; /* Alterado para flex-start */
   align-items: center;
 }
 
-.date {
-  color: #666;
-  margin-left: 0.5rem; /* Adicionada margem esquerda */
+.reaction {
+  display: flex;
+  align-items: center;
+  margin-right: 10px; /* Adicionando um espaço entre os botões */
 }
 
-.like-button {
-  background-color: #007bff;
-  color: #fff;
+.like-button,
+.deslike-button {
+  background-color: transparent;
   border: none;
-  padding: 5px 10px;
-  border-radius: 5px;
   cursor: pointer;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.like-count,
+.deslike-count {
+  margin-left: 5px;
+  font-family: 'Inter', sans-serif;
+  font-size: 14px;
+  color: #333333 ;
+  font-weight: lighter;
+}
+
+.rotated {
+  transform: rotate(180deg);
 }
 </style>
--- vue-app/src/pages/AvaliacaoPage.vue
+++ vue-app/src/pages/AvaliacaoPage.vue
@@ -1,75 +1,91 @@
 <template>
-    <div class="avaliacoes">
-      <NavBar />
-      <h1 class="titulo-avaliacao">Avaliações</h1>
-        <p class="reviews"> 12 Comentários</p>
-      <div class="listagem-avaliacoes">
-        <CardMinhaAvaliacao v-for="avaliacao in avaliacoes" :key="avaliacao.id" :avaliacao="avaliacao" />
-      </div>
-      
+  <div class="avaliacoes">
+    <NavBar />
+    <h1 class="titulo-avaliacao">Avaliações</h1>
+    <p class="reviews">{{ avaliacoes.length }} Comentários</p>
+    <div class="listagem-avaliacoes">
+      <CardMinhaAvaliacao v-for="avaliacao in avaliacoes" :key="avaliacao.id" :avaliacao="avaliacao" />
     </div>
     <FooterBar />
-  </template>
-  
-  <script>
-  import NavBar from '@/components/Navegacao/NavBar.vue';
-  import FooterBar from '@/components/Navegacao/FooterBar.vue';
-  import CardMinhaAvaliacao from '@/components/Avaliacao/CardMinhaAvaliacao.vue';
-  
-  export default {
-    name: 'AvaliacaoPage',
-    components: {
-      NavBar,
-      FooterBar,
-      CardMinhaAvaliacao,
-    },
-    data() {
-      return {
-        avaliacoes: [
-          { id: 1, titulo: 'Avaliação 1', nome_usuario: 'Usuário 1', nota: 4.5, detalhes: 'Detalhes da avaliação 1' },
-          { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
-          // Adicione mais avaliações conforme necessário
-        ],
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .avaliacoes {
-    background: hsla(209, 63%, 17%, 1);
-    background: linear-gradient(90deg, hsla(209, 63%, 17%, 1) 0%, hsla(183, 71%, 16%, 1) 100%);
-    background: -moz-linear-gradient(90deg, hsla(209, 63%, 17%, 1) 0%, hsla(183, 71%, 16%, 1) 100%);
-    background: -webkit-linear-gradient(90deg, hsla(209, 63%, 17%, 1) 0%, hsla(183, 71%, 16%, 1) 100%);
-    width: 100vw;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100vh;
-    text-decoration: white;
-  }
-  .titulo-avaliacao {
-    color: #E0E0E0;
-    font-family: 'Open Sans', sans-serif;
-    font-size: 64px;
-    font-weight: bold;
-  }
+  </div>
+</template>
 
-  .reviews {
-    color: #BFBFBF;
-    font-family:'Inter', sans-serif;
-    font-size: 16px;
-  }
-  
-  .listagem-avaliacoes {
-    margin-top: 30px;
-    width: 100%;
-    max-width: 1200px; /* Ajuste conforme necessário */
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 20px;
+<script>
+import NavBar from '@/components/Navegacao/NavBar.vue';
+import FooterBar from '@/components/Navegacao/FooterBar.vue';
+import CardMinhaAvaliacao from '@/components/Avaliacao/CardMinhaAvaliacao.vue';
 
-  } 
-  </style>
-  
+export default {
+  name: 'AvaliacaoPage',
+  components: {
+    NavBar,
+    FooterBar,
+    CardMinhaAvaliacao,
+  },
+  data() {
+    return {
+      avaliacoes: [
+        { id: 1, titulo: 'Avaliação 1', nome_usuario: 'Usuário 1', nota: 4.5, detalhes: 'Detalhes da avaliação 1' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+        { id: 2, titulo: 'Avaliação 2', nome_usuario: 'Usuário 2', nota: 3.8, detalhes: 'Detalhes da avaliação 2' },
+
+
+        // Adicione mais avaliações conforme necessário
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.avaliacoes {
+  background: hsla(209, 63%, 17%, 1);
+  background: linear-gradient(90deg, hsla(209, 63%, 17%, 1) 0%, hsla(183, 71%, 16%, 1) 100%);
+  background: -moz-linear-gradient(90deg, hsla(209, 63%, 17%, 1) 0%, hsla(183, 71%, 16%, 1) 100%);
+  background: -webkit-linear-gradient(90deg, hsla(209, 63%, 17%, 1) 0%, hsla(183, 71%, 16%, 1) 100%);
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: white;
+  min-height: 100vh; /* altura mínima da página */
+}
+
+.titulo-avaliacao {
+  color: #E0E0E0;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 64px;
+  font-weight: bold;
+  margin-top: 20px;
+}
+
+.reviews {
+  color: #BFBFBF;
+  font-family:'Inter', sans-serif;
+  font-size: 16px;
+}
+
+.listagem-avaliacoes {
+  margin-top: 30px;
+  width: 100%;
+  max-width: 834px; /* Ajuste conforme necessário */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  padding-bottom: 100px; /* espaço para o FooterBar */
+  box-sizing: border-box; /* incluir padding na altura */
+}
+
+.FooterBar {
+  margin-top: auto; /* empurrar para a parte inferior */
+}
+</style>
