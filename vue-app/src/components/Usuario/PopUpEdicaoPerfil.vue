13ed8b7c2c9fbdbe360c5e8d4818294e2a01802f
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-06-29 21:50:04 -0300

    fix: ajustado o loading no perfil

--- vue-app/src/components/Usuario/PopUpEdicaoPerfil.vue
+++ vue-app/src/components/Usuario/PopUpEdicaoPerfil.vue
@@ -1,64 +1,64 @@
 <template>
-	<div class="popup-wrapper">
-		<div class="popup">
-			<h2>Edite seu perfil</h2>
-			<form @submit.prevent="submitForm">
-				<div class="form-group">
-					<label for="photoUrl">URL da Foto</label>
-					<input
-						maxlength="500"
-						type="url"
-						id="photoUrl"
-						v-model="form.foto_url"
-					/>
-				</div>
-				<div class="form-group">
-					<label for="firstName">Nome</label>
-					<input
-						maxlength="100"
-						type="text"
-						id="firstName"
-						v-model="form.nome"
-					/>
-				</div>
-				<div class="form-group">
-					<label for="lastName">Sobrenome</label>
-					<input
-						maxlength="100"
-						type="text"
-						id="lastName"
-						v-model="form.sobrenome"
-					/>
-				</div>
-				<div class="form-group">
-					<label for="email">Email</label>
-					<input maxlength="100" type="email" id="email" v-model="form.email" />
-				</div>
-				<div class="form-group">
-					<label for="dropdown">Curso</label>
-					<select id="dropdown" v-model="form.curso">
-						<option value="" disabled selected>Escolha uma opção</option>
-						<option value="Engenharia de Software">
-							Engenharia de Software
-						</option>
-						<option value="Engenharia de Energia">Engenharia de Energia</option>
-						<option value="Engenharia Eletrônica">Engenharia Eletrônica</option>
-						<option value="Engenharia Aeroespacial">
-							Engenharia Aeroespacial
-						</option>
-						<option value="Engenharia Automotiva">Engenharia Automotiva</option>
-					</select>
-				</div>
-				<div class="buttons">
-					<button type="submit">Salvar</button>
-					<button class="cancelar" @click="closePopUp">Cancelar</button>
-				</div>
-				<div v-if="error_message" class="error-message">
-					{{ error_message }}
-				</div>
-			</form>
-		</div>
-	</div>
+  <div class="popup-wrapper">
+    <div class="popup">
+      <h2>Edite seu perfil</h2>
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <label for="photoUrl">URL da Foto</label>
+          <input
+            maxlength="500"
+            type="url"
+            id="photoUrl"
+            v-model="form.foto_url"
+          />
+        </div>
+        <div class="form-group">
+          <label for="firstName">Nome</label>
+          <input
+            maxlength="100"
+            type="text"
+            id="firstName"
+            v-model="form.nome"
+          />
+        </div>
+        <div class="form-group">
+          <label for="lastName">Sobrenome</label>
+          <input
+            maxlength="100"
+            type="text"
+            id="lastName"
+            v-model="form.sobrenome"
+          />
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input maxlength="100" type="email" id="email" v-model="form.email" />
+        </div>
+        <div class="form-group">
+          <label for="dropdown">Curso</label>
+          <select id="dropdown" v-model="form.curso">
+            <option value="" disabled selected>Escolha uma opção</option>
+            <option value="Engenharia de Software">
+              Engenharia de Software
+            </option>
+            <option value="Engenharia de Energia">Engenharia de Energia</option>
+            <option value="Engenharia Eletrônica">Engenharia Eletrônica</option>
+            <option value="Engenharia Aeroespacial">
+              Engenharia Aeroespacial
+            </option>
+            <option value="Engenharia Automotiva">Engenharia Automotiva</option>
+          </select>
+        </div>
+        <div class="buttons">
+          <button type="submit">Salvar</button>
+          <button class="cancelar" @click="closePopUp">Cancelar</button>
+        </div>
+        <div v-if="error_message" class="error-message">
+          {{ error_message }}
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -66,181 +66,182 @@ import { editarDadosPerfil } from "@/service/usuario/editarDadosPerfil";
 import { getUsuarios } from "@/repositories/usuario/obterUsuarios.js";
 import { verificarEmail, verificarUrl } from "@/generals/verificarInputs.js";
 export default {
-	name: "PopUpEdicaoPerfil",
-	props: {
-		usuarioInfo: Object,
-	},
-	data() {
-		return {
-			form: {
-				foto_url: this.usuarioInfo.foto_url ? this.usuarioInfo.foto_url : "",
-				nome: this.usuarioInfo ? this.usuarioInfo.nome : "",
-				sobrenome: this.usuarioInfo ? this.usuarioInfo.sobrenome : "",
-				email: this.usuarioInfo ? this.usuarioInfo.email : "",
-				curso: this.usuarioInfo ? this.usuarioInfo.curso : "",
-			},
-			error_message: "",
-		};
-	},
-	methods: {
-		closePopUp() {
-			this.$emit("close-popup");
-		},
+  name: "PopUpEdicaoPerfil",
+  props: {
+    usuarioInfo: Object,
+  },
+  data() {
+    return {
+      form: {
+        foto_url: this.usuarioInfo.foto_url ? this.usuarioInfo.foto_url : "",
+        nome: this.usuarioInfo ? this.usuarioInfo.nome : "",
+        sobrenome: this.usuarioInfo ? this.usuarioInfo.sobrenome : "",
+        email: this.usuarioInfo ? this.usuarioInfo.email : "",
+        curso: this.usuarioInfo ? this.usuarioInfo.curso : "",
+      },
+      error_message: "",
+    };
+  },
+  methods: {
+    closePopUp() {
+      this.$emit("close-popup");
+    },
 
-		async submitForm() {
-			if (this.verificarInputs()) {
-				if (!(await this.verificaExistencia(this.form.email))) {
-					try {
-						const usuarioAtualizado = {
-							matricula: this.usuarioInfo.matricula,
-							nome: this.form.nome,
-							sobrenome: this.form.sobrenome,
-							email: this.form.email,
-							foto_url: this.form.foto_url,
-							curso: this.form.curso,
-						};
-						await editarDadosPerfil(usuarioAtualizado);
-						sessionStorage.setItem("foto_perfil", usuarioAtualizado.foto_url);
-						this.$emit("dados-atualizados", usuarioAtualizado);
-						this.closePopUp();
-					} catch (error) {
-						console.error("Erro ao atualizar usuário:", error);
-					}
-				} else {
-					this.error_message = "Esse email já existe! Escolha outro email.";
-				}
-			} else return;
-		},
+    async submitForm() {
+      if (this.verificarInputs()) {
+        if (!(await this.verificaExistencia(this.form.email))) {
+          try {
+            const usuarioAtualizado = {
+              matricula: this.usuarioInfo.matricula,
+              nome: this.form.nome,
+              sobrenome: this.form.sobrenome,
+              email: this.form.email,
+              foto_url: this.form.foto_url,
+              curso: this.form.curso,
+            };
+            await editarDadosPerfil(usuarioAtualizado);
+            sessionStorage.setItem("foto_perfil", usuarioAtualizado.foto_url);
+            this.$emit("dados-atualizados", usuarioAtualizado);
+            this.closePopUp();
+          } catch (error) {
+            console.error("Erro ao atualizar usuário:", error);
+          }
+        } else {
+          this.error_message = "Esse email já existe! Escolha outro email.";
+        }
+      } else return;
+    },
 
-		async verificaExistencia(email) {
-			const usuarios = await getUsuarios();
-			for (let i = 0; i < usuarios.length; i++) {
-				if (usuarios[i].email === email && this.usuarioInfo.email != email)
-					return true;
-			}
-			return false;
-		},
+    async verificaExistencia(email) {
+      const usuarios = await getUsuarios();
+      for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].email === email && this.usuarioInfo.email != email)
+          return true;
+      }
+      return false;
+    },
 
-		verificarInputs() {
-			if (!verificarEmail(this.form.email)) {
-				this.error_message = "Insira um email válido.";
-				return false;
-			}
-			if (!verificarUrl(this.form.foto_url)) {
-				this.error_message = "Insira uma url de foto válida";
-				return false;
-			}
+    verificarInputs() {
+      if (!verificarEmail(this.form.email)) {
+        this.error_message = "Insira um email válido.";
+        return false;
+      }
+      if (!verificarUrl(this.form.foto_url)) {
+        this.error_message = "Insira uma url de foto válida";
+        return false;
+      }
 
-			return true;
-		},
-	},
+      return true;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .popup-wrapper {
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background: rgba(0, 0, 0, 0.5);
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 h2 {
-	font-family: "Open Sans", sans-serif;
-	font-size: 3rem;
-	color: #0a745b;
+  font-family: "Open Sans", sans-serif;
+  font-size: 3rem;
+  color: #0a745b;
 }
 .popup {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	background: white;
-	padding-top: 40px;
-	padding-bottom: 30px;
-	border-radius: 20px;
-	box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-	max-width: 500px;
-	width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: white;
+  padding-top: 40px;
+  padding-bottom: 30px;
+  border-radius: 20px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  max-width: 500px;
+  width: 90%;
 }
 form {
-	width: 90%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 .form-group {
-	width: 90%;
-	margin-bottom: 20px;
+  width: 90%;
+  margin-bottom: 20px;
 }
 .form-group label {
-	display: block;
-	margin-bottom: 5px;
-	font-family: "Open Sans", sans-serif;
-	font-size: 1.4rem;
+  display: block;
+  margin-bottom: 5px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.4rem;
 }
 .form-group input,
 .form-group select {
-	width: 100%;
-	padding: 8px;
-	box-sizing: border-box;
-	border: solid 1px #c9c9c9;
-	border-radius: 10px;
+  width: 100%;
+  padding: 8px;
+  box-sizing: border-box;
+  border: solid 1px #c9c9c9;
+  border-radius: 10px;
 }
 
 .form-group input:focus,
 .form-group select:focus {
-	outline: none;
+  outline: none;
 }
 .buttons {
-	display: flex;
-	width: 90%;
-	justify-content: space-between;
+  display: flex;
+  width: 90%;
+  justify-content: space-between;
 }
 button {
-	border: none;
-	width: 48%;
-	display: flex;
-	padding: 8px 0px 6px 0;
-	border-radius: 6px;
-	background-color: #0a745b;
-	align-items: center;
-	justify-content: center;
-	color: white;
-	font-size: 1.4rem;
-	font-family: "Open Sans", sans-serif;
-	transition: 100ms;
+  cursor: pointer;
+  border: none;
+  width: 48%;
+  display: flex;
+  padding: 8px 0px 6px 0;
+  border-radius: 6px;
+  background-color: #0a745b;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 1.4rem;
+  font-family: "Open Sans", sans-serif;
+  transition: 100ms;
 }
 
 .error-message {
-	font-family: "Open Sans", sans-serif;
-	margin-top: 20px;
-	font-size: 1.6rem;
+  font-family: "Open Sans", sans-serif;
+  margin-top: 20px;
+  font-size: 1.6rem;
 }
 
 .cancelar {
-	border: none;
-	width: 48%;
-	display: flex;
-	padding: 8px 0px 6px 0;
-	border-radius: 6px;
-	background: none;
-	align-items: center;
-	justify-content: center;
-	color: #0a745b;
-	border: solid 1px #0a745b;
-	font-size: 1.4rem;
-	font-family: "Open Sans", sans-serif;
-	transition: 100ms;
+  border: none;
+  width: 48%;
+  display: flex;
+  padding: 8px 0px 6px 0;
+  border-radius: 6px;
+  background: none;
+  align-items: center;
+  justify-content: center;
+  color: #0a745b;
+  border: solid 1px #0a745b;
+  font-size: 1.4rem;
+  font-family: "Open Sans", sans-serif;
+  transition: 100ms;
 }
 .cancelar:hover {
-	background: none;
+  background: none;
 }
 button:hover {
-	background: #075543;
+  background: #075543;
 }
 </style>
--- vue-app/src/pages/PerfilPage.vue
+++ vue-app/src/pages/PerfilPage.vue
@@ -1,63 +1,64 @@
 <template>
-	<div class="profile-wraper">
-		<LoadingComponent v-if="loading" :isLoading="loading"/>
-		<div v-if="!loading" class="card-profile">
-			<img
-				src="../assets/icons/navegacao/back_arrow.svg"
-				alt="back-arrow"
-				class="back-arrow"
-				@click="back"
-			/>
-			<div class="profile-picture-and-profile-name">
-				<div class="profile-picture-wraper">
-					<img
-						:src="verificarUrl(userInfo.foto_url)"
-						@error="carregarImgAlternativa"
-						alt="Profile-Picture"
-						class="profile-picture"
-					/>
-				</div>
-				<div class="profile-name-and-profile-course">
-					<h2 class="profile-name">{{ userInfo.nome }}</h2>
-					<p class="profile-course">{{ userInfo.curso }}</p>
-				</div>
-			</div>
-			<div class="quantity-avaliations">
-				<div class="professor-avaliations">
-					<h2 class="quantity-professor">
-						{{ userInfo.quantidade_professores_avaliados }}
-					</h2>
-					<p>Professores avaliados</p>
-				</div>
-				<div class="course-avaliations">
-					<h2 class="quantity-course">
-						{{ userInfo.quantidade_materias_avaliadas }}
-					</h2>
-					<p>Matérias avaliadas</p>
-				</div>
-			</div>
-			<div class="infos">
-				<h2>Meus dados</h2>
-				<div class="data">
-					<p><span>Email:</span> {{ userInfo.email }}</p>
-					<p><span>Matrícula:</span> {{ userInfo.matricula }}</p>
-					<p><span>Universidade:</span> UNB Faculdade do Gama</p>
-				</div>
-			</div>
-			<div class="log-out-and-edit">
-				<button class="edit" @click="togglePopUpEdit">Editar</button>
-				<button class="log-out" @click="handleLogOut">
-					<img src="../assets/icons/perfil/log-out-icon.svg" alt="" />Log-out
-				</button>
-			</div>
-		</div>
-		<PopUpEdicaoPerfil
-			v-if="togglePopUp"
-			@close-popup="togglePopUpEdit"
-			@dados-atualizados="atualizarDadosUsuario"
-			:usuarioInfo="userInfo"
-		/>
-	</div>
+  <div class="profile-wraper">
+    <div class="card-profile">
+      <LoadingComponent class="loading" v-if="loading" :isLoading="loading" />
+      <img
+        v-if="!loading"
+        src="../assets/icons/navegacao/back_arrow.svg"
+        alt="back-arrow"
+        class="back-arrow"
+        @click="back"
+      />
+      <div v-if="!loading" class="profile-picture-and-profile-name">
+        <div class="profile-picture-wraper">
+          <img
+            :src="verificarUrl(userInfo.foto_url)"
+            @error="carregarImgAlternativa"
+            alt="Profile-Picture"
+            class="profile-picture"
+          />
+        </div>
+        <div class="profile-name-and-profile-course">
+          <h2 class="profile-name">{{ userInfo.nome }}</h2>
+          <p class="profile-course">{{ userInfo.curso }}</p>
+        </div>
+      </div>
+      <div v-if="!loading" class="quantity-avaliations">
+        <div class="professor-avaliations">
+          <h2 class="quantity-professor">
+            {{ userInfo.quantidade_professores_avaliados }}
+          </h2>
+          <p>Professores avaliados</p>
+        </div>
+        <div class="course-avaliations">
+          <h2 class="quantity-course">
+            {{ userInfo.quantidade_materias_avaliadas }}
+          </h2>
+          <p>Matérias avaliadas</p>
+        </div>
+      </div>
+      <div v-if="!loading" class="infos">
+        <h2>Meus dados</h2>
+        <div class="data">
+          <p><span>Email:</span> {{ userInfo.email }}</p>
+          <p><span>Matrícula:</span> {{ userInfo.matricula }}</p>
+          <p><span>Universidade:</span> UNB Faculdade do Gama</p>
+        </div>
+      </div>
+      <div v-if="!loading" class="log-out-and-edit">
+        <button class="edit" @click="togglePopUpEdit">Editar</button>
+        <button class="log-out" @click="handleLogOut">
+          <img src="../assets/icons/perfil/log-out-icon.svg" alt="" />Log-out
+        </button>
+      </div>
+    </div>
+    <PopUpEdicaoPerfil
+      v-if="togglePopUp"
+      @close-popup="togglePopUpEdit"
+      @dados-atualizados="atualizarDadosUsuario"
+      :usuarioInfo="userInfo"
+    />
+  </div>
 </template>
 
 <script>
@@ -65,282 +66,288 @@ import { getInfoUserProfileService } from "@/service/usuario/getInfoUserProfile"
 import PopUpEdicaoPerfil from "@/components/Usuario/PopUpEdicaoPerfil.vue";
 import router from "../routes/index";
 import LoadingComponent from "@/components/Navegacao/LoadingComponent.vue";
-export default {
-	components: {
-		PopUpEdicaoPerfil,
-		LoadingComponent,
-	},
-	name: "ProfilePage",
-	data() {
-		return {
-			userInfo: {},
-			togglePopUp: false,
-			loading: true
-		};
-	},
 
-	methods: {
-		back() {
-			router.go(-1);
-		},
-		handleLogOut() {
-			sessionStorage.setItem("matricula", "");
-			sessionStorage.setItem("foto_perfil", "");
-			sessionStorage.setItem("professores_avaliados", "");
-			sessionStorage.setItem("materias_avaliadas", "");
-			sessionStorage.setItem("likes_dislikes_materias", "");
-			sessionStorage.setItem("likes_dislikes_professores", "");
-			window.location.reload();
-		},
-		togglePopUpEdit() {
-			this.togglePopUp = !this.togglePopUp;
-		},
-		verificarUrl(url_profile_picture) {
-			if (url_profile_picture === "" || url_profile_picture === null) {
-				return "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-avatar-profile-picture-male-icon.png";
-			}
-			return url_profile_picture;
-		},
-		atualizarDadosUsuario(dadosAtualizados) {
-			this.userInfo = { ...this.userInfo, ...dadosAtualizados };
-		},
-		carregarImgAlternativa(event) {
-			event.target.src =
-				"https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-avatar-profile-picture-male-icon.png";
-		},
-	},
-	mounted() {
-		this.loading = true
-		getInfoUserProfileService()
-			.then((userInfo) => {
-				this.userInfo = userInfo;
-				this.loading = false
-			})
-			.catch((erro) => {
-				console.error("Erro ao obter meus dados", erro);
-			});
-	},
+export default {
+  components: {
+    PopUpEdicaoPerfil,
+    LoadingComponent,
+  },
+  name: "ProfilePage",
+  data() {
+    return {
+      userInfo: {},
+      togglePopUp: false,
+      loading: true,
+    };
+  },
+
+  methods: {
+    back() {
+      router.go(-1);
+    },
+    handleLogOut() {
+      sessionStorage.setItem("matricula", "");
+      sessionStorage.setItem("foto_perfil", "");
+      sessionStorage.setItem("professores_avaliados", "");
+      sessionStorage.setItem("materias_avaliadas", "");
+      sessionStorage.setItem("likes_dislikes_materias", "");
+      sessionStorage.setItem("likes_dislikes_professores", "");
+      window.location.reload();
+    },
+    togglePopUpEdit() {
+      this.togglePopUp = !this.togglePopUp;
+    },
+    verificarUrl(url_profile_picture) {
+      if (url_profile_picture === "" || url_profile_picture === null) {
+        return "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-avatar-profile-picture-male-icon.png";
+      }
+      return url_profile_picture;
+    },
+    atualizarDadosUsuario(dadosAtualizados) {
+      this.userInfo = { ...this.userInfo, ...dadosAtualizados };
+    },
+    carregarImgAlternativa(event) {
+      event.target.src =
+        "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-avatar-profile-picture-male-icon.png";
+    },
+  },
+  mounted() {
+    this.loading = true;
+    getInfoUserProfileService()
+      .then((userInfo) => {
+        this.userInfo = userInfo;
+        this.loading = false;
+      })
+      .catch((erro) => {
+        console.error("Erro ao obter meus dados", erro);
+      });
+  },
 };
 </script>
 
 <style scoped>
+.loading {
+  padding: 250px 0px 250px 0px;
+}
 .profile-wraper {
-	background: hsla(209, 63%, 17%, 1);
-	background: linear-gradient(
-		90deg,
-		hsla(209, 63%, 17%, 1) 0%,
-		hsla(183, 71%, 16%, 1) 100%
-	);
-	background: -moz-linear-gradient(
-		90deg,
-		hsla(209, 63%, 17%, 1) 0%,
-		hsla(183, 71%, 16%, 1) 100%
-	);
-	background: -webkit-linear-gradient(
-		90deg,
-		hsla(209, 63%, 17%, 1) 0%,
-		hsla(183, 71%, 16%, 1) 100%
-	);
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	text-decoration: white;
-	height: 100vh;
-	min-height: fit-content;
+  background: hsla(209, 63%, 17%, 1);
+  background: linear-gradient(
+    90deg,
+    hsla(209, 63%, 17%, 1) 0%,
+    hsla(183, 71%, 16%, 1) 100%
+  );
+  background: -moz-linear-gradient(
+    90deg,
+    hsla(209, 63%, 17%, 1) 0%,
+    hsla(183, 71%, 16%, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    90deg,
+    hsla(209, 63%, 17%, 1) 0%,
+    hsla(183, 71%, 16%, 1) 100%
+  );
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-decoration: white;
+  height: 100vh;
+  min-height: fit-content;
 }
 
 .card-profile {
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: flex-start;
-	width: 80%;
-	max-width: 700px;
-	height: fit-content;
-	background-color: rgb(245, 245, 245);
-	border-radius: 40px;
-	box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.055);
-	gap: 5vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 80%;
+  max-width: 700px;
+  height: fit-content;
+  background-color: rgb(245, 245, 245);
+  border-radius: 40px;
+  box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.055);
+  gap: 5vh;
 }
 
 .back-arrow {
-	position: absolute;
-	top: 50px;
-	left: 50px;
-	width: 2rem;
+  cursor: pointer;
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  width: 2rem;
 }
 
 .profile-picture-and-profile-name {
-	width: 80%;
-	margin-top: 5vh;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 25px;
+  width: 80%;
+  margin-top: 5vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
 }
 
 .profile-picture-wraper {
-	padding: 2.4px;
-	width: 16rem;
-	height: 16rem;
-	border-radius: 50%;
-	background: rgb(8, 54, 80);
-	background: linear-gradient(150deg, #1b5994 40%, #0a745b 60%);
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  padding: 2.4px;
+  width: 16rem;
+  height: 16rem;
+  border-radius: 50%;
+  background: rgb(8, 54, 80);
+  background: linear-gradient(150deg, #1b5994 40%, #0a745b 60%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .profile-picture {
-	width: 100%;
-	height: 100%;
-	object-fit: cover;
-	border-radius: 50%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
 }
 
 .profile-name-and-profile-course {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 }
 
 .profile-name {
-	font-family: "Open Sans", sans-serif;
-	font-size: 3.4rem;
-	font-weight: 700;
-	color: #0a745b;
-	margin: 0;
+  font-family: "Open Sans", sans-serif;
+  font-size: 3.4rem;
+  font-weight: 700;
+  color: #0a745b;
+  margin: 0;
 }
 
 .profile-course {
-	font-family: "Open Sans", sans-serif;
-	font-size: 1.6rem;
-	font-weight: 500;
-	color: #474747;
-	margin: 5px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: #474747;
+  margin: 5px;
 }
 
 .quantity-avaliations {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 }
 
 .professor-avaliations {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	width: 40%;
-	border-right: solid 1px #47474760;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40%;
+  border-right: solid 1px #47474760;
 }
 
 .course-avaliations {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	width: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40%;
 }
 
 .quantity-professor,
 .quantity-course {
-	font-family: "Open Sans", sans-serif;
-	font-size: 2.4rem;
-	font-weight: 600;
-	color: #0a745b;
-	margin: 0;
+  font-family: "Open Sans", sans-serif;
+  font-size: 2.4rem;
+  font-weight: 600;
+  color: #0a745b;
+  margin: 0;
 }
 
 .quantity-avaliations p {
-	font-family: "Open Sans", sans-serif;
-	font-size: 1.4rem;
-	font-weight: 500;
-	color: #474747;
-	margin: 5px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: #474747;
+  margin: 5px;
 }
 
 .infos {
-	width: 80%;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	justify-content: flex-start;
-	gap: 3vh;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 3vh;
 }
 
 .data {
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .infos h2 {
-	font-family: "Open Sans", sans-serif;
-	font-size: 3rem;
-	font-weight: 700;
-	color: #525252;
-	margin: 0;
+  font-family: "Open Sans", sans-serif;
+  font-size: 3rem;
+  font-weight: 700;
+  color: #525252;
+  margin: 0;
 }
 
 .infos span {
-	font-family: "Open Sans", sans-serif;
-	font-size: 1.8rem;
-	font-weight: 600;
-	color: #8a8a8a;
-	margin-top: 5px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.8rem;
+  font-weight: 600;
+  color: #8a8a8a;
+  margin-top: 5px;
 }
 
 .infos p {
-	font-family: "Open Sans", sans-serif;
-	font-size: 1.8rem;
-	font-weight: 500;
-	color: #646464;
-	margin-top: 5px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.8rem;
+  font-weight: 500;
+  color: #646464;
+  margin-top: 5px;
 }
 
 .log-out-and-edit {
-	width: 80%;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	margin-bottom: 50px;
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 50px;
 }
 
 .log-out {
-	border: none;
-	width: 48%;
-	display: flex;
-	padding: 14px 0px 14px 0;
-	border-radius: 10px;
-	align-items: center;
-	justify-content: center;
-	color: #0a745b;
-	gap: 10px;
-	font-size: 1.8rem;
-	font-family: "Open Sans", sans-serif;
-	background: none;
-	border: solid 1px #0a745b;
-	cursor: pointer;
+  border: none;
+  width: 48%;
+  display: flex;
+  padding: 14px 0px 14px 0;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  color: #0a745b;
+  gap: 10px;
+  font-size: 1.8rem;
+  font-family: "Open Sans", sans-serif;
+  background: none;
+  border: solid 1px #0a745b;
+  cursor: pointer;
 }
 
 .log-out img {
-	width: 2rem;
+  width: 2rem;
 }
 
 .edit {
-	border: none;
-	width: 48%;
-	display: flex;
-	padding: 14px 0px 14px 0;
-	border-radius: 10px;
-	background-color: #0a745b;
-	align-items: center;
-	justify-content: center;
-	color: white;
-	font-size: 1.8rem;
-	font-family: "Open Sans", sans-serif;
+  cursor: pointer;
+  border: none;
+  width: 48%;
+  display: flex;
+  padding: 14px 0px 14px 0;
+  border-radius: 10px;
+  background-color: #0a745b;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 1.8rem;
+  font-family: "Open Sans", sans-serif;
 }
 </style>
